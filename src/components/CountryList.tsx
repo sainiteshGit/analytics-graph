e72ce3c303a5d440f13887989e378166f897a301
sainiteshGit <nitesh_4u@yahoo.com>
2022-11-14 21:18:09 -0500

    added a new item component

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Country } from "../types";
+import CountryItem from "./CountryItem";
 
 interface Props{
     countries: Country[];
@@ -12,21 +13,7 @@ display: flex;
 flex-wrap: wrap;
 `;
 
-const ListItem = styled.li`
-    list-style-type: none;
-    flex: 0 0 50%;
-    text-align: center;
-    cursor: pointer;
-    @media (min-width: 740px){
-        flex: 0 0 10%;
-    }
-`;
 
-const ListContent= styled.div`
-background-color: #f7f7f7;
-margin: 5px;
-padding: 10px 0;
-`
 
 const CountryList: React.FunctionComponent<Props> = ({
     countries,
@@ -36,14 +23,7 @@ const CountryList: React.FunctionComponent<Props> = ({
     return (
         <ListWrapper>
             {countries?.map((country)=>(
-                <ListItem key={country.ID} onClick={()=>onItemClick(country)}>
-                 <ListContent>
-                        <h4>{country.Country}</h4>
-                        <div>New Confirmed: {country.NewConfirmed}</div>
-                        <div>New Deaths: {country.NewDeaths}</div>
-                        <div>New Recovered: {country.NewRecovered}</div>
-                 </ListContent>
-                </ListItem>
+                <CountryItem country={country} onItemClick={onItemClick}/>
             ))}
         </ListWrapper>
     );
