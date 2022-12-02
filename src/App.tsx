1eed5b82eaa16115ded049bca391a62d40dc9157
sainiteshGit <nitesh_4u@yahoo.com>
2022-12-01 19:40:05 -0500

    udpating to include the barchart for removing the recovered

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const App:React.FunctionComponent = () => {
           <hr/>
           {activeCountries.length?<BarChart countries={activeCountries} type=''/>:null}
           {activeCountries.length?<BarChart countries={activeCountries} type='deaths'/>:null}
-          {activeCountries.length?<BarChart countries={activeCountries} type='recovered'/>:null}
+          {/* {activeCountries.length?<BarChart countries={activeCountries} type='recovered'/>:null} */}
 
 
           <CountryList countries={data.Countries} onItemClick={onCountryClick}/>
